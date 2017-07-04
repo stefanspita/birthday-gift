31622b73eded77d36d843c2317177100629b8e90
Stefano <stefan.spita@bluespeckfinancial.co.uk>
2017-07-04 15:57:50 +0100

    Add initial love default

--- src/app/pages/home/reducer.js
+++ src/app/pages/home/reducer.js
@@ -1,10 +1,12 @@
 import R from "ramda"
 import initialBudget from "../../../data/initial-budget"
+import initialLove from "../../../data/initial-love"
 import consts from "../../../constants"
 
 const initialState = {
   buttonClicked: false,
   budget: initialBudget,
+  love: initialLove,
 }
 
 export default function reducer(state = initialState, action) {
