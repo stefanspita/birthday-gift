078e643fc0567417531adbd3468006be81d9f8fa
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-07-14 09:19:27 +0100

    Mark gifts that are out of budget

--- src/app/pages/home/reducer.js
+++ src/app/pages/home/reducer.js
@@ -4,16 +4,32 @@ import initialLove from "../../../data/initial-love"
 import {gifts as allAvailableGifts, categories} from "../../../data/initial-gifts"
 import consts from "../../../constants"
 
+function calculateBudgetAfterSale (currency, state, gift) {
+  return R.useWith(R.subtract, [R.path(["budget", currency]), R.path(["cost", currency])])(state, gift)
+}
+
+const checkIfEnoughBudget = R.curry((budget, gift) => {
+  const keys = R.keys(initialBudget)
+  for(let i=0;i<keys.length;i++) {
+    const key = keys[i]
+    if (R.lt(budget[key], gift.cost[key])) return R.merge(gift, {outOfBudget: true})
+  }
+  return gift
+})
+
+function updateGiftsState(availableBudget, availableGifts, giftId) {
+  return R.compose(
+    R.map(checkIfEnoughBudget(availableBudget)),
+    R.reject(R.propEq("id", giftId))
+  )(availableGifts)
+}
+
 const initialState = {
   budget: initialBudget,
   love: initialLove,
-  availableGifts: allAvailableGifts,
+  availableGifts: updateGiftsState(initialBudget, allAvailableGifts, -1),
   categories: categories,
   cart: [],
-}
-
-function calculateBudgetAfterSale (currency, state, gift) {
-  return R.useWith(R.subtract, [R.path(["budget", currency]), R.path(["cost", currency])])(state, gift)
 }
 
 export default function reducer(state = initialState, action) {
@@ -21,15 +37,17 @@ export default function reducer(state = initialState, action) {
   case consts.ADD_GIFT_TO_CART_SUCCESS: {
     const {giftId} = action.payload
     const gift = R.find(R.propEq("id", giftId), state.availableGifts)
+    const newBudget = {
+      peopleOverload: calculateBudgetAfterSale("peopleOverload", state, gift),
+      energy: calculateBudgetAfterSale("energy", state, gift),
+      money: calculateBudgetAfterSale("money", state, gift),
+    }
+
     return R.merge(state, {
       cart: R.append(gift, state.cart),
-      availableGifts: R.reject(R.propEq("id", giftId), state.availableGifts),
+      availableGifts: updateGiftsState(newBudget, state.availableGifts, giftId),
       love: state.love + gift.love,
-      budget: {
-        peopleOverload: calculateBudgetAfterSale("peopleOverload", state, gift),
-        energy: calculateBudgetAfterSale("energy", state, gift),
-        money: calculateBudgetAfterSale("money", state, gift),
-      },
+      budget: newBudget,
     })
   }
   default:
