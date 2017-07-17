75128ea0050972a4eecdd14b699eff63b64f08b9
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-07-17 14:00:35 +0100

    Display cart totals

--- src/app/pages/home/components/cart/index.js
+++ src/app/pages/home/components/cart/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import R from "ramda"
 import PropTypes from "prop-types"
+import {Currencies} from "../../../../components"
 import {CartItem} from "./item"
+import styles from "./cart.css"
 
 function renderCartItem(props) {
   return (<CartItem
@@ -10,18 +12,29 @@ function renderCartItem(props) {
   />)
 }
 
-export function Cart({cart}) {
+export function Cart({cart, cartTotal}) {
   if (cart.length === 0) return null
   return (
     <div>
       <h2>Shopping cart</h2>
-      {R.map(renderCartItem, cart)}
+      <div className={styles.cart}>
+        <div>{R.map(renderCartItem, cart)}</div>
+        <div>
+          <h4>Total</h4>
+          <Currencies
+            peopleOverload={cartTotal.peopleOverload}
+            energy={cartTotal.energy}
+            money={cartTotal.money}
+          />
+        </div>
+      </div>
     </div>
   )
 }
 
 Cart.propTypes = {
   cart: PropTypes.array.isRequired,
+  cartTotal: PropTypes.object.isRequired,
 }
 
 renderCartItem.propTypes = {
