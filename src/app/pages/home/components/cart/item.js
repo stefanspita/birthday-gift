29b80e14c9692bf9c24b08456f2720dbe2d9f4e2
Stefano <stefan.spita@bluespeckfinancial.co.uk>
2017-07-20 10:09:02 +0100

    Add a remove from cart icon

--- src/app/pages/home/components/cart/item.js
+++ src/app/pages/home/components/cart/item.js
@@ -3,18 +3,21 @@ import PropTypes from "prop-types"
 import {Currencies} from "../../../../components"
 import styles from "./item.css"
 
-// export function CartItem({name, cost}) {
-export function CartItem({cost}) {
-  // <label>{name}</label>
+export function CartItem({name, cost}) {
   return (
     <div className={styles.item}>
-      <label>Label</label>
+      <div className={styles.removeIcon}>
+        <i className="fa fa-minus-circle"></i>
+      </div>
       <div>
-        <Currencies
-          peopleOverload={cost.peopleOverload}
-          energy={cost.energy}
-          money={cost.money}
-        />
+        <label>{name}</label>
+        <div>
+          <Currencies
+            peopleOverload={cost.peopleOverload}
+            energy={cost.energy}
+            money={cost.money}
+          />
+        </div>
       </div>
     </div>
   )
