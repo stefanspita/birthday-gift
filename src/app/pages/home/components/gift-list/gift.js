f2c6cf4d57e352da89498404657775061a60d99d
Stefano <stefan.spita@bluespeckfinancial.co.uk>
2017-07-21 12:59:14 +0100

    Replace the gift names with a generic gift picture

--- src/app/pages/home/components/gift-list/gift.js
+++ src/app/pages/home/components/gift-list/gift.js
@@ -2,20 +2,19 @@ import React from "react"
 import PropTypes from "prop-types"
 import {Currencies} from "../../../../components"
 import styles from "./gift.css"
-// import giftPictureSrc from "./assets/gift-outline-md.png"
+import giftPictureSrc from "./assets/gift-outline-md.png"
 
-export function Gift({id, name, cost, addToCart, outOfBudget}) {
+export function Gift({id, cost, addToCart, outOfBudget}) {
   let giftClass = styles.wrapper
   if (outOfBudget) giftClass = styles.disabledWrapper
-  // <div className={styles.giftImage}>
-  //   <img src={giftPictureSrc}></img>
-  //   </div>
   return (
     <div
       className={giftClass}
       onClick={() => addToCart(id)}
     >
-      <label>{name}</label>
+      <div>
+        <img className={styles.giftImage} src={giftPictureSrc}></img>
+      </div>
       <div>
         <Currencies
           peopleOverload={cost.peopleOverload}
@@ -29,7 +28,6 @@ export function Gift({id, name, cost, addToCart, outOfBudget}) {
 
 Gift.propTypes = {
   id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
   cost: PropTypes.object.isRequired,
   outOfBudget: PropTypes.bool,
   addToCart: PropTypes.func.isRequired,
