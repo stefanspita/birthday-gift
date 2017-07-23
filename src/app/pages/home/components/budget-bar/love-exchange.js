b922351a7645d974b9dd0769c52f4843ef386ad4
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-07-23 16:46:07 +0100

    Plug currency exchange actions into the buttons

--- src/app/pages/home/components/budget-bar/love-exchange.js
+++ src/app/pages/home/components/budget-bar/love-exchange.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import {Currencies} from "../../../../components"
 import styles from "./love-exchange.css"
 
-export function LoveExchange({peopleOverload, energy, money}) {
+export function LoveExchange({peopleOverload, energy, money, sellCurrency, buyCurrency}) {
   return (
     <div
       className={styles.exchangeModal}
@@ -15,16 +15,16 @@ export function LoveExchange({peopleOverload, energy, money}) {
       />
       <div className={styles.currencyExchangeWrapper}>
         <div className={styles.buttonGroupWrapper}>
-          <button className={styles.positive}>+</button>
-          <button className={styles.negative}>-</button>
+          <button className={styles.positive} onClick={() => buyCurrency("peopleOverload")}>+</button>
+          <button className={styles.negative} onClick={() => sellCurrency("peopleOverload")}>-</button>
         </div>
         <div className={styles.buttonGroupWrapper}>
-          <button className={styles.positive}>+</button>
-          <button className={styles.negative}>-</button>
+          <button className={styles.positive} onClick={() => buyCurrency("energy")}>+</button>
+          <button className={styles.negative} onClick={() => sellCurrency("energy")}>-</button>
         </div>
         <div className={styles.buttonGroupWrapper}>
-          <button className={styles.positive}>+</button>
-          <button className={styles.negative}>-</button>
+          <button className={styles.positive} onClick={() => buyCurrency("money")}>+</button>
+          <button className={styles.negative} onClick={() => sellCurrency("money")}>-</button>
         </div>
       </div>
     </div>)
