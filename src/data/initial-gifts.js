4c15b78271462d5a9b2213cb229841dc8540fa9b
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-07-24 12:49:05 +0100

    Adjust gift costs to make the currency system more flexible

--- src/data/initial-gifts.js
+++ src/data/initial-gifts.js
@@ -5,27 +5,27 @@ export const categories = [
   {id: 4, category: "Writing"},
   {id: 5, category: "The things I do for you"},
   {id: 6, category: "Cooking for you"},
-  {id: 7, category: "Doing something for me to get more currency"},
+  {id: 7, category: "Getting more currency"},
 ]
 export const gifts = [
-  {id: 1, name: "Cornwall", cost: {peopleOverload: 100, energy: 100, money: 200}, love: 80, imageName: "", categoryId: 1},
+  {id: 1, name: "Cornwall", cost: {peopleOverload: 120, energy: 100, money: 200}, love: 80, imageName: "", categoryId: 1},
   {id: 2, name: "Kingsbridge and Devon", cost: {peopleOverload: 50, energy: 100, money: 150}, love: 150, imageName: "", categoryId: 1},
-  {id: 3, name: "Indonesia trip to see elepharnts", cost: {peopleOverload: 350, energy: 350, money: 700}, love: 300, imageName: "", categoryId: 1},
-  {id: 4, name: "Bruges weekend trip", cost: {peopleOverload: 200, energy: 200, money: 300}, love: 80, imageName: "", categoryId: 1},
-  {id: 5, name: "Amsterdam weekend trip", cost: {peopleOverload: 250, energy: 200, money: 300}, love: 120, imageName: "", categoryId: 1},
-  {id: 6, name: "Berlin trip", cost: {peopleOverload: 250, energy: 200, money: 300}, love: 100, imageName: "", categoryId: 1},
+  {id: 3, name: "Indonesia trip to see elepharnts", cost: {peopleOverload: 300, energy: 400, money: 600}, love: 200, imageName: "", categoryId: 1},
+  {id: 4, name: "Bruges weekend trip", cost: {peopleOverload: 200, energy: 200, money: 200}, love: 80, imageName: "", categoryId: 1},
+  {id: 5, name: "Amsterdam weekend trip", cost: {peopleOverload: 250, energy: 200, money: 250}, love: 150, imageName: "", categoryId: 1},
+  {id: 6, name: "Berlin trip", cost: {peopleOverload: 250, energy: 200, money: 200}, love: 100, imageName: "", categoryId: 1},
 
   {id: 7, name: "Giant handwarmer", cost: {peopleOverload: 0, energy: 0, money: 15}, love: 20, imageName: "", categoryId: 2},
   {id: 8, name: "Any book you want", cost: {peopleOverload: 0, energy: 0, money: 10}, love: 0, imageName: "", categoryId: 2},
   {id: 9, name: "Any house plant you want", cost: {peopleOverload: 0, energy: 0, money: 40}, love: 20, imageName: "", categoryId: 2},
   {id: 10, name: "Laptop", cost: {peopleOverload: 0, energy: 0, money: 200}, love: -200, imageName: "", categoryId: 2},
-  {id: 11, name: "A new car", cost: {peopleOverload: 0, energy: 300, money: 2000}, love: -300, imageName: "", categoryId: 2},
+  {id: 11, name: "A new car", cost: {peopleOverload: 0, energy: 300, money: 1500}, love: -300, imageName: "", categoryId: 2},
   {id: 25, name: "Golden snitch fidget spinner", cost: {peopleOverload: 0, energy: 0, money: 3}, love: 0, imageName: "", categoryId: 2},
 
-  {id: 17, name: "Creative writing course", cost: {peopleOverload: 0, energy: 0, money: 350}, love: 100, imageName: "", categoryId: 4},
-  {id: 18, name: "Program a brand new blog for you, however you want it", cost: {peopleOverload: 0, energy: 400, money: 30}, love: 100, imageName: "", categoryId: 4},
+  {id: 17, name: "Creative writing course", cost: {peopleOverload: 0, energy: 0, money: 300}, love: 100, imageName: "", categoryId: 4},
+  {id: 18, name: "Program a brand new blog for you, however you want it", cost: {peopleOverload: 0, energy: 400, money: 30}, love: 150, imageName: "", categoryId: 4},
 
-  {id: 19, name: "Full flat clean", cost: {peopleOverload: 0, energy: 50, money: 0}, love: -100, imageName: "", categoryId: 5},
+  {id: 19, name: "Full flat clean", cost: {peopleOverload: 0, energy: 50, money: 0}, love: -50, imageName: "", categoryId: 5},
   {id: 20, name: "Make you tea without complaining for a week", cost: {peopleOverload: 0, energy: 30, money: 0}, love: -50, imageName: "", categoryId: 5},
   {id: 21, name: "Get myself a new pillow", cost: {peopleOverload: 20, energy: 20, money: 20}, love: -20, imageName: "", categoryId: 5},
 
@@ -35,11 +35,12 @@ export const gifts = [
 
   {id: 12, name: "Kayaking in Forrest of Dean", cost: {peopleOverload: 20, energy: 100, money: 50}, love: 20, imageName: "", categoryId: 3},
   {id: 13, name: "Bristol paddle boarding", cost: {peopleOverload: 100, energy: 70, money: 30}, love: -30, imageName: "", categoryId: 3},
-  {id: 14, name: "Chew valley country farm", cost: {peopleOverload: 80, energy: 20, money: 30}, love: 100, imageName: "", categoryId: 3},
+  {id: 14, name: "Chew valley country farm", cost: {peopleOverload: 50, energy: 20, money: 30}, love: 100, imageName: "", categoryId: 3},
   {id: 15, name: "Couple's relaxing massage", cost: {peopleOverload: 30, energy: 0, money: 60}, love: 70, imageName: "", categoryId: 3},
   {id: 16, name: "Go karting", cost: {peopleOverload: 30, energy: 50, money: 60}, love: -30, imageName: "", categoryId: 3},
 
   {id: 26, name: "Make me all the teas for a week", cost: {peopleOverload: 0, energy: 0, money: 0}, love: 30, imageName: "", categoryId: 7},
   {id: 27, name: "Let me teach you how to defend in fifa", cost: {peopleOverload: 0, energy: 0, money: 0}, love: 60, imageName: "", categoryId: 7},
   {id: 28, name: "Play tennis with me", cost: {peopleOverload: 0, energy: 0, money: 0}, love: 40, imageName: "", categoryId: 7},
+  {id: 29, name: "Wear my bristol city t-shirt when we go out on time", cost: {peopleOverload: 0, energy: 0, money: 0}, love: 60, imageName: "", categoryId: 7},
 ]
